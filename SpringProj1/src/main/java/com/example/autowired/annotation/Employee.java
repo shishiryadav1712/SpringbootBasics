dab14acc1ae40ecb6b7b07358e2ef523e93babbc
shishiryadav1712 <shishiry1712@gmail.com>
2025-04-21 22:13:53 -0400

    Spring Final

---
+++ SpringProj1/src/main/java/com/example/autowired/annotation/Employee.java
@@ -0,0 +1,58 @@
+package com.example.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+@Component("employee")
+public class  Employee {
+
+    @Value("11211")
+    private int employeeId;
+    @Value("Jack")
+    private String firstName;
+    @Value("Daniels")
+    private String lastName;
+    @Value("#{34*33}")
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
